67f0562736cfec72d7c9f42a8cdf8d528deb392b
Pascal Martineau <pascal@lewebsimple.ca>
2024-09-17 09:21:34 -0400

    feat: style the speaker icon before element

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,13 @@
 import './style.css'
 
-console.log(`Click-speech `);
+document.querySelectorAll('[data-click-speech]').forEach((element) => {
+
+  element.addEventListener('click', () => {
+    const text = element.textContent;
+    if (!text) return;
+    const utterance = new SpeechSynthesisUtterance(text);
+    utterance.lang = 'fr-FR';
+    speechSynthesis.speak(utterance);
+  });
+
+});
