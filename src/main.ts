c6e6db2a0546a15ee460f397b11acf2eb37ccf55
Pascal Martineau <pascal@lewebsimple.ca>
2024-09-17 09:32:54 -0400

    feat: Detect click on :before pseudo element

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,24 @@ import './style.css'
 
 document.querySelectorAll('[data-click-speech]').forEach((element) => {
 
-  element.addEventListener('click', () => {
-    const text = element.textContent;
-    if (!text) return;
-    const utterance = new SpeechSynthesisUtterance(text);
-    utterance.lang = 'fr-FR';
-    speechSynthesis.speak(utterance);
+  element.addEventListener('click', (event) => {
+    const target = event.currentTarget || event.target;
+    if (!target) return;
+    const before = getComputedStyle(<Element>target, ":before");
+    if (before) {
+      const top = Number(before.getPropertyValue('top').slice(0, -2));
+      const left = Number(before.getPropertyValue('left').slice(0, -2));
+      const width = Number(before.getPropertyValue('width').slice(0, -2));
+      const height = Number(before.getPropertyValue('height').slice(0, -2));
+      const x = (<MouseEvent>event).layerX;
+      const y = (<MouseEvent>event).layerY;
+      if (x >= left && x <= left + width && y >= top && y <= top + height) {
+        const text = element.textContent;
+        if (!text) return;
+        const utterance = new SpeechSynthesisUtterance(text);
+        utterance.lang = 'fr-FR';
+        speechSynthesis.speak(utterance);
+      }
+    }
   });
-
 });
