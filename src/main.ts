8a43d7c92f99c37fa42a57a33f188e6b105efee8
Pascal Martineau <pascal@lewebsimple.ca>
2024-09-17 13:14:18 -0400

    feat: grow click zone by 10px

--- src/main.ts
+++ src/main.ts
@@ -11,10 +11,10 @@ document.querySelectorAll('[data-click-speech]').forEach((element) => {
     if (!target) return;
     const before = getComputedStyle(<Element>target, ":before");
     if (before) {
-      const top = Number(before.getPropertyValue('top').slice(0, -2));
-      const left = Number(before.getPropertyValue('left').slice(0, -2));
-      const width = Number(before.getPropertyValue('width').slice(0, -2));
-      const height = Number(before.getPropertyValue('height').slice(0, -2));
+      const top = Number(before.getPropertyValue('top').slice(0, -2)) + 10;
+      const left = Number(before.getPropertyValue('left').slice(0, -2)) - 10;
+      const width = Number(before.getPropertyValue('width').slice(0, -2)) + 20;
+      const height = Number(before.getPropertyValue('height').slice(0, -2)) + 20;
       const x = (<MouseEvent>event).layerX;
       const y = (<MouseEvent>event).layerY;
       if (x >= left && x <= left + width && y >= top && y <= top + height) {
